8e987b69b3811b0104e41531c6f7125a8fc539b2
mdachi-codes <jessejustinm@gmail.com>
2020-01-08 09:21:45 +0300

    add success & fail actions to todos.actions

--- src/app/Store/Actions/todos.action.ts
+++ src/app/Store/Actions/todos.action.ts
@@ -3,12 +3,20 @@ import { Todo } from "src/app/models/todo.model";
 //import { type } from "os";
 
 export const getTodosAction = createAction("[Home Page] GetTodos");
+export const getTodosSuccessAction = createAction(
+  "[Home Page] GetTodosSuccess"
+);
+export const getTodosFailureAction = createAction(
+  "[Home Page] GetTodosFailure"
+);
 
 export const addTodoAction = createAction(
   "[Home Page] AddTodo",
   props<{ todo: Todo }>()
 );
-
-export const loadTodoAction = createAction("[Home Page] load todo");
+export const addTodoSuccessAction = createAction("[Home Page] AddTodoSuccess");
+export const addTodoFailureAction = createAction("[Home Page] AddTodoFailure");
 
 export const deleteTodoAction = createAction("[Home Page] DeleteTodo");
+export const deleteTodoSuccessAction = createAction("[Home Page] DeleteTodo");
+export const deleteTodoFailureAction = createAction("[Home Page] DeleteTodo");
